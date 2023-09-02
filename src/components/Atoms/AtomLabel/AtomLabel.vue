f22f77f62275911b41e24583db0a49593618d6a4
Kheuval <hadrien.hartstein@gmail.com>
2023-09-02 18:49:35 +0200

    [Feature] Add Atomlabel component

---
+++ src/components/Atoms/AtomLabel/AtomLabel.vue
@@ -0,0 +1,16 @@
+<template>
+  <label :class="classNames" :for="id">
+    {{ content }}
+  </label>
+</template>
+
+<script lang="ts" setup>
+import type { ClassNames } from "../AtomPropsTypes";
+
+defineProps<
+  ClassNames & {
+    id: string;
+    content: string;
+  }
+>();
+</script>
